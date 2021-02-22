892d5e541ae0b36ea8bee21797169141319cebdd
Roy Leung <roy.leung@gravitas.com.hk>
2021-02-22 15:05:06 +0800

    add need method

--- flutter_currency/lib/api/HistoryResponse.dart
+++ flutter_currency/lib/api/HistoryResponse.dart
@@ -7,4 +7,11 @@ class HistoryResponse {
   String base;
   String date;
   Map<String, Map<String, double>> rates;
+
+  HistoryResponse({this.base, this.date, this.rates});
+
+  factory HistoryResponse.fromJson(Map<String, dynamic> json) =>
+      _$HistoryResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$HistoryResponseToJson(this);
 }
