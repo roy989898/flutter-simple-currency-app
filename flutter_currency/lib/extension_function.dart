0a5e640a80e8e938627472be124e848f24f8f145
Roy Leung <roy.leung@gravitas.com.hk>
2021-02-22 16:17:00 +0800

    save

--- flutter_currency/lib/extension_function.dart
+++ flutter_currency/lib/extension_function.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 ImageFlag(String country) {
-  var usedName = country.toLowerCase()[0] + country.toLowerCase()[1];
+  var usedName = getTheCorrectFlagName(country);
   return Image.asset('icons/flags/png/$usedName.png', package: 'country_icons');
 }
 
 ImageFlag25X(String country) {
-  var usedName = country.toLowerCase()[0] + country.toLowerCase()[1];
+  var usedName = getTheCorrectFlagName(country);
   return Image.asset('icons/flags/png/2.5x/$usedName.png',
       package: 'country_icons');
+}
+
+getTheCorrectFlagName(String country) {
+  try {
+    var usedName = country.toLowerCase()[0] + country.toLowerCase()[1];
+    return usedName;
+  } catch (e) {
+    return '';
+  }
 }
